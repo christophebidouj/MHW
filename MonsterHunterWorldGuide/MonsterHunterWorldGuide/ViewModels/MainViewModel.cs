6df2c354e99e68f379311377b00cc6ba1ecf7802
christophe.bidouj <christophe.bidouj@gmail.com>
2018-10-11 19:22:09 +0200

    Ajoutez des fichiers projet.

---
+++ MonsterHunterWorldGuide/MonsterHunterWorldGuide/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonsterHunterWorldGuide.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+        public List<Monster> Monsters
+        {
+            get;
+            set;
+        }
+
+        public MainViewModel()
+        {
+            Monsters = new List<Monster>();
+        }
+    }
+
+    public class Monster
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
